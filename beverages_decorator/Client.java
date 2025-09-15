132acf0dfc5dacc0fc1cc3be3d8f6d1f4158e24a
Daksh <daksh@Dakshs-MacBook.local>
2025-09-15 19:51:50 +0530

    beverages_decorator decorator-exercises-starter

---
+++ beverages_decorator/Client.java
@@ -0,0 +1,21 @@
+package beverages_decorator;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+
+		
+		Beverage coffee = new Cappuccino();
+        System.out.println("Coffee");
+		System.out.println(coffee.cost());
+        coffee = new ChocolateDecorator(coffee);
+        System.out.println(coffee.cost());
+        coffee = new ChocolateDecorator(coffee);
+        System.out.println(coffee.cost());
+		
+		
+
+	}
+
+}
